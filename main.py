15ff3d2c799323d024ff1b84a727bc00afcd3792
Chigozie Oduah <oduahchigozie46@gmail.com>
2023-11-19 20:39:49 +0100

    built project

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, send_file, request
+
+app = Flask(__name__, static_url_path="/")
+
+name = {"text": "Apple bean"}
+
+
+@app.route("/")
+def welcome():
+    return send_file("./static/index.html")
+
+
+@app.get("/api/name")
+def get_name():
+    return name["text"]
+
+
+@app.post("/api/name")
+def set_name():
+    print(request.data)
+    name["text"] = request.data
+    return name["text"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
